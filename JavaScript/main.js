9c2d7b3c878024f52c339213ed08896ec2afd680
JimRohn <genuaf@lsbu.ac.uk>
2023-11-27 21:15:35 +0000

    Added Tami.me logo and updated styles for consistency

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,3 +15,29 @@ function filterPeople() {
         }
     }
 }
+
+function showPersonCard(personId) {
+    // Hide the people list
+    document.getElementById('peopleList').style.display = 'none';
+
+    // Logic to load the person card details based on the personId
+    // For now, just show the person card
+    var personCard = document.getElementById('personCard');
+    // Here you can set the innerHTML of the person card with the details of the clicked person
+    // For demonstration, we'll just display it
+    personCard.style.display = 'block';
+    
+    // This is where you would update the person card's information based on the personId
+    // For example:
+    // personCard.querySelector('.person-name').textContent = 'Name based on personId';
+    // personCard.querySelector('.person-role').textContent = 'Role based on personId';
+    // ... and so on for other fields
+}
+function showPersonCard() {
+    document.getElementById('personCard').style.display = 'block';
+}
+
+function hidePersonCard() {
+    document.getElementById('personCard').style.display = 'none';
+}
+
